96f44df78041dfab416b53bad9b5cf06863a830d
shmurda26 <124792879+shmurda26@users.noreply.github.com>
2024-05-23 11:14:59 +0200

    Add project files.

---
+++ Parkfinder.DataBase/dbo/Tables/Parkingdb.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Parkingdb] (
+    [Antalparkeringer]   INT NOT NULL,
+    [Optagetparkeringer] INT NOT NULL,
+    [Ledigeparkeringer]  INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Antalparkeringer] ASC)
+);
+
